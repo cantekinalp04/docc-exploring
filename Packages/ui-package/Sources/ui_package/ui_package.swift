435cba6bce46e2323ff09399eb85879447b87308
cantekinalp <cantekinalp@gmail.com>
2023-02-05 02:31:29 +0300

    update

--- Packages/ui-package/Sources/ui_package/ui_package.swift
+++ Packages/ui-package/Sources/ui_package/ui_package.swift
@@ -3,7 +3,7 @@ public struct UIPackage {
     public init() {}
     /// Returns text view with supplied text
     /// - Parameter text: a `String` value
-    /// - Returns: ``Text`` view with passed text
+    /// - Returns: `Text` view with passed text
     public func textView(text: String) -> some View {
         Text(text)
     }
